b1135167646b019bc7038298515da5a5389c740a
Ariadne Rangel <ranbenitezjl@gmail.com>
2021-01-20 13:35:12 -0600

    Add JS <fNumbers>

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,9 @@ function press(){
     let name = document.getElementById("name").value;
     alert("Lear git is cool, isn`t it " + name + "?");
 }
+
+function numbers() {
+    for (let i = 1; i <= 10; i++) {
+        document.writeln(i);
+    }
+}
